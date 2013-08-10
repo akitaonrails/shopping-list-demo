398fece9b1e60b3c903d07ee1e1588b91f8b7437
Fabio Akita <fabioakita@gmail.com>
2013-08-10 14:29:57 -0300

    initial setup with bootstrap and simple form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoppingListDemo::Application.config.secret_token = '6faeb399e3da93533c42558ece449fa69cb96d4d901bbc27299d57e1d258c4dc167fc5934300dfb33efce709a6a55cc1535dfbcd2b51d6b87e55bd360849e889'
