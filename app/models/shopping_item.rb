bbf26fe8e20063cb42cd9a2ecd0c02a485767196
Fabio Akita <fabioakita@gmail.com>
2013-08-10 15:08:42 -0300

    Bug introduced!

--- app/models/shopping_item.rb
+++ app/models/shopping_item.rb
@@ -2,7 +2,11 @@ class ShoppingItem < ActiveRecord::Base
   belongs_to :shopping_list
   attr_accessible :name, :price, :quantity
 
-  def total
+  def sub_total
     quantity * price
+  end
+
+  def total
+    sub_total + ( sub_total * shopping_list.tax_rate )
   end
 end
